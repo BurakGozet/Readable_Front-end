b263067e871d2c25169e7781a34f343c05ab7073
Burak Gozet <gozetburak@gmail.com>
2015-12-01 20:45:33 +0200

    BurakGozet

---
+++ _assets/js/components/custom.js
@@ -0,0 +1,3 @@
+$(".search_toggle").click(function (){
+    alert("test");
+});
--- gruntfile.js
+++ gruntfile.js
@@ -86,7 +86,6 @@
 });
 
    // Load tasks
-   grunt.loadNpmTasks('grunt-contrib-clean');
    grunt.loadNpmTasks('grunt-contrib-uglify');
    grunt.loadNpmTasks('grunt-contrib-watch');
    grunt.loadNpmTasks('grunt-recess');
