debc9381abb0dd83b6146a82f8e4ca0d9058c27e
Burak Gozet <gozetburak@gmail.com>
2015-12-02 15:18:23 +0200

    BurakGozet

--- _assets/js/components/custom.js
+++ _assets/js/components/custom.js
@@ -1,3 +1,14 @@
-$(".search_toggle").click(function (){
-    alert("test");
+$(document).ready(function(){
+ $(".search-toggle").click(function (){
+    $(".search-panel").toggle();
+    $(".page-content-container").toggleClass("blur10");
+    $("#search-input").focus();
+});
+ $(".search-panel").keypress(function(e){
+    alert(e.which);
+    if(e.which == 27){
+     $(".search-panel").toggle();
+     $(".page-content-container").toggleClass("blur10");
+ }
+
 });
