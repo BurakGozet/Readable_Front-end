c54b56b0b0b13e402144028e0e1fbcf302aa7ce9
Talha Aydemir <talha.aydemir@metglobal.com>
2015-12-04 16:09:19 +0200

    search focus fixed

--- _assets/js/components/custom.js
+++ _assets/js/components/custom.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	$(".search-toggle").click(function (){
 		$(".search-panel").toggleClass("show");
 		$(".page-content-container").toggleClass("blur10");
-		$("#search-input").trigger("click");
+		document.getElementById('search-input').focus();
 	});
 
 	$("body").keydown(function(e){
